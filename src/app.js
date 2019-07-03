855feb1cf23f2547eb9d63b73409e6cd069310bf
Rodrigo <rcoto@protonmail.com>
2019-07-03 00:45:22 -0700

    Style/HTML: responsive navbar

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 import "./styles/styles.scss";
+
+// =====================================================
+// NABVAR===============================================
+// =====================================================
+
+// Grab list, item, link
+const list = document.querySelector(".navigation__list");
+const items = document.querySelectorAll(".navigation__item");
+const links = document.querySelectorAll(".navigation__link");
+const nav_btn = document.querySelector(".navigation__button");
+
+const active_list = "navigation__active-list";
+const active_item = "navigation__active-item";
+const active_link = "navigation__active-link";
+const hide = "hide";
+
+// Will add the class hide to navigation__nav
+
+nav_btn.addEventListener("click", () => {
+  // Single element
+  list.classList.toggle(active_list);
+  list.classList.toggle(hide);
+  // multiple elements
+  items.forEach(item => item.classList.toggle(active_item));
+  links.forEach(link => link.classList.toggle(active_link));
+});
