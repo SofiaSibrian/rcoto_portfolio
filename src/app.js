bbf438d084f2b0c66cb4d7463deaab9438b20d34
Rodrigo <rcoto@protonmail.com>
2019-07-04 01:24:04 -0700

    Feat: navbar selection partially working

--- src/app.js
+++ src/app.js
@@ -25,3 +25,43 @@ nav_btn.addEventListener("click", () => {
   items.forEach(item => item.classList.toggle(active_item));
   links.forEach(link => link.classList.toggle(active_link));
 });
+
+// =====================================================
+// Observers============================================
+// =====================================================
+const nav = document.querySelector("#navigation");
+const header = document.querySelector("#header");
+const qualifications = document.querySelector("#qualifications");
+const portfolio = document.querySelector("#portfolio");
+const accomplishments = document.querySelector("#accomplishments");
+const about = document.querySelector("#about");
+const footer = document.querySelector("#footer");
+
+const navHome = document.querySelector("#nav-home");
+const navQuad = document.querySelector("#nav-qualifications");
+const navPort = document.querySelector("#nav-portfolio");
+const navAcc = document.querySelector("#nav-accomplishments");
+const navContact = document.querySelector("#nav-about");
+
+const navHightlight = (element, link) => {
+  const navOpt = {};
+  const navObserver = new IntersectionObserver((entries, sectionObserver) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        // Out of section
+        link.classList.remove("selected");
+      } else {
+        // In section
+        link.classList.add("selected");
+      }
+    });
+  }, navOpt);
+
+  navObserver.observe(element);
+};
+
+navHightlight(header, navHome);
+navHightlight(qualifications, navQuad);
+navHightlight(portfolio, navPort);
+navHightlight(accomplishments, navAcc);
+navHightlight(about, navContact);
